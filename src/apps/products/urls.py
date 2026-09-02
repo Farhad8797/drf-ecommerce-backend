22fbb749f486066228c86efd5bc7cc7a14e93061
Farhad8797 <gdvbhbhb@gmail.com>
2026-09-02 19:03:38 +0600

    Created urls.py for products

--- src/apps/products/urls.py
+++ src/apps/products/urls.py
@@ -11,15 +11,15 @@ from django.urls import path
 
 urlpatterns = [
     path('category/<str:product_type>/', CategoryView.as_view({'get':'retrieve'}), name='category'),
-    path('product/<str:slug>/', ProductReadView.as_view({'get':'retrieve'}), name='product'),
+    path('product/<slug:slug>/', ProductReadView.as_view({'get':'retrieve'}), name='product'),
     path('product-variant/<int:pk>/', ProductVariantReadView.as_view({'get':'retrieve'}), name='product_variant'),
     path('c/product/', ProductCreateUpdateDeleteRetrieveViewSet.as_view({'get':'list', 'post':'create'}), name='created_product'),
-    path('u/product/<str:slug>/', 
+    path('u/product/<slug:slug>/', 
          ProductCreateUpdateDeleteRetrieveViewSet.as_view({'get':'retrieve', 'patch':'partial_update', 'put':'update', 'delete':'destroy'}), 
          name='updated_product'
     ),
     path('c/product-variant/', ProductVariantCreateListViewSet.as_view({'get':'list', 'post':'create'}), name='created_product_variant'),
-    path('u/product-variant/<int:pk>', 
+    path('u/product-variant/<int:pk>/', 
          ProductVariantUpdateRetrieveDeleteViewSet.as_view({'get':'retrieve', 'patch':'partial_update', 'put':'update', 'delete':'destroy'}), 
          name='updated_product_variant'
     )
